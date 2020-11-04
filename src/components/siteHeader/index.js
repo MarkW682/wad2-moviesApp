baf07a18da1609d56be3c248d40b7b48a8a43715
Mark White <markwhitestudent@hotmail.com>
2020-11-04 18:15:08 +0000

    Change site header

---
+++ src/components/siteHeader/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../globals/fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./siteHeader.css";
+
+const SiteHeader = () => {
+  return (
+    <nav className="navbar  navbar-light fixed-top  bg-dark ">
+      <nav className="navbar-brand text-white">
+        <Link className=" text-white" to="/">
+          TMDB Client
+        </Link>
+      </nav>
+      <FontAwesomeIcon
+        className="navbar-text text-light"
+        icon={["fas", "video"]}
+        size="3x"
+      />
+      <span className="navbar-text text-light">
+        For the movie enthusiast !!
+      </span>
+      <FontAwesomeIcon
+        className="navbar-text text-light"
+        icon={["fas", "film"]}
+        size="3x"
+      />
+      <nav className="navbar navbar-expand ">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/movies/favorites">
+              Favorites
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </nav>
+  );
+};
+
+export default SiteHeader;
--- src/index.js
+++ src/index.js
@@ -6,24 +6,14 @@ import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
 import FavoriteMoviesPage from './pages/favoritesMoviesPage'
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader'
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="jumbotron">
-      <div className="container-fluid">
-        <ul className="navbar-nav text-black">
-          <li className="nav-item">
-            <Link className="nav-link " to="/">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link " to="/movies/favorites">
-              Favorites
-            </Link>
-          </li>
-        </ul>
+      <div className="jumbotron">
+          <SiteHeader />
+          <div className="container-fluid">
         <Switch>
           <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
